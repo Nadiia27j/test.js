47846c48878dce76599b0f7b1c1c0bd375dfedb7
Надія Ткач <nadiya77723@gmail.com>
2022-08-29 17:44:34 +0300

    jjjkj

--- js/index-test.js
+++ js/index-test.js
@@ -781,25 +781,27 @@
 
 
 
-const select = document.querySelector(".pizza-select");
-const textOutput = document.querySelector(".text-output");
-const valueOutput = document.querySelector(".value-output");
+// const select = document.querySelector(".pizza-select");
+// const textOutput = document.querySelector(".text-output");
+// const valueOutput = document.querySelector(".value-output");
 
-setOutput();
+// setOutput();
 
-select.addEventListener("change", setOutput);
+// select.addEventListener("change", setOutput);
 
-function setOutput() {
-  const selectedOptionValue = select.value;
-  const selectedOptionIndex = select.selectedIndex;
-  const selectedOptionText = select.options[selectedOptionIndex].text;
+// function setOutput() {
+//   const selectedOptionValue = select.value;
+//   const selectedOptionIndex = select.selectedIndex;
+//   const selectedOptionText = select.options[selectedOptionIndex].text;
 
-  textOutput.textContent = selectedOptionText;
-  valueOutput.textContent = selectedOptionValue;
-}
+//   textOutput.textContent = selectedOptionText;
+//   valueOutput.textContent = selectedOptionValue;
+// }
 
 
-console.log(selectedOptionValue );
+// console.log(selectedOptionValue );
+
+
 
 
 
